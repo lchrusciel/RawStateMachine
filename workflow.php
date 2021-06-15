c19f65af4da42c4e7b606b9f9e91c23f347156a4
Łukasz Chruściel <lchrusciel@gmail.com>
2021-06-16 00:27:11 +0200

    [Workflow] Blocking transition

--- workflow.php
+++ workflow.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\EventListener\BlockedGuardListener;
 use App\EventListener\PaymentPaidListener;
 use App\Model\Payment;
 use Symfony\Component\EventDispatcher\EventDispatcher;
@@ -11,11 +12,12 @@ use Symfony\Component\Workflow\Workflow;
 require __DIR__ . '/vendor/autoload.php';
 
 $definitionBuilder = new DefinitionBuilder();
-$definition = $definitionBuilder->addPlaces(['new','pending','failed','paid'])
+$definition = $definitionBuilder->addPlaces(['new','pending','failed','paid','blocked'])
     // Transitions are defined with a unique name, an origin place and a destination place
     ->addTransition(new Transition('process', 'new', 'pending'))
     ->addTransition(new Transition('fail', 'pending', 'failed'))
     ->addTransition(new Transition('pay', 'pending', 'paid'))
+    ->addTransition(new Transition('block', 'pending', 'blocked'))
     ->build()
 ;
 
@@ -25,12 +27,16 @@ $marking = new MethodMarkingStore($singleState, $property);
 
 $dispatcher = new EventDispatcher();
 $dispatcher->addListener('workflow.payment.enter.paid', new PaymentPaidListener());
+$dispatcher->addListener(
+    'workflow.payment.guard.block',
+    new BlockedGuardListener()
+);
 
 $workflow = new Workflow($definition, $marking, $dispatcher, 'payment');
 
 $payment = new Payment();
 
 $workflow->apply($payment, 'process');
-$workflow->apply($payment, 'pay');
+$workflow->apply($payment, 'block');
 
 var_dump($payment);
